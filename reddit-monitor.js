011660403a52ab075cbed1b77bfcda9609163154
Greg Bateman <gerbot150@gmail.com>
2018-03-04 18:35:10 -0500

    Timer now pauses when leaving the page and persists in the session through reloads and links

--- reddit-monitor.js
+++ reddit-monitor.js
@@ -1,3 +1,4 @@
+// ************** Styling **************
 var container = document.createElement('div');
 var text = document.createElement('div');
 document.body.appendChild(container);
@@ -14,20 +15,47 @@ container.style.color = 'white';
 
 text.innerHTML = '00:00';
 
-var startDate = Date.now();
+// ************** Logic **************
+var startDate = sessionStorage.getItem('reddit-monitor-start-date');
+if (startDate == null) {
+  startDate = Date.now();
+}
 
-setInterval(function() {
-  var elapsed = Date.now() - startDate;
-  var hours = Math.floor(elapsed/3600000).toFixed(0);
-  var minutes = Math.floor(elapsed%3600000/60000).toFixed(0);
-  var seconds = Math.floor(elapsed%60000/1000).toFixed(0);
-  text.innerHTML = ('0' + minutes).substring(minutes.length - 1) + ':'
-    + ('0' + seconds).substring(seconds.length - 1);
-  if(hours > 0) {
-    text.innerHTML = hours + ':' + text.innerHTML;
-  }
+var paused = false;
+var pausedTime = 0;
+var pauseStartTime = null;
 
-  if(minutes % 5 == 0 && seconds == 0 && !document.hidden) {
-    alert('You have spent ' + (60*hours + minutes) + ' minutes on reddit');
+function updateClock() {
+  if (!paused) {
+    var elapsed = Date.now() - startDate - pausedTime;
+    var hours = Math.floor(elapsed/3600000).toFixed(0);
+    var minutes = Math.floor(elapsed%3600000/60000).toFixed(0);
+    var seconds = Math.floor(elapsed%60000/1000).toFixed(0);
+    text.innerHTML = ('0' + minutes).substring(minutes.length - 1) + ':'
+      + ('0' + seconds).substring(seconds.length - 1);
+    if(hours > 0) {
+      text.innerHTML = hours + ':' + text.innerHTML;
+    }
+
+    if(minutes % 5 == 0 && seconds == 0 && elapsed != 0) {
+      alert('You have spent ' + (60*hours + minutes) + ' minutes on reddit');
+    }
   }
-}, 1000);
+}
+
+updateClock();
+setInterval(updateClock, 1000);
+
+document.addEventListener("visibilitychange", function() {
+  if (document.hidden) {
+    pauseStartTime = Date.now();
+    paused = true;
+  } else {
+    pausedTime += Date.now() - pauseStartTime;
+    paused = false;
+  }
+});
+
+window.onbeforeunload = function(e) {
+  sessionStorage.setItem('reddit-monitor-start-date', startDate);
+}
